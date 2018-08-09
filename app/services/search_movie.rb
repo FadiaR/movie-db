03619a503dde14e93b2ac3a49fca8d7c86886e62
Fadia Rostane <39888576+FadiaR@users.noreply.github.com>
2018-08-10 01:57:17 +0200

    en pls

--- app/services/search_movie.rb
+++ app/services/search_movie.rb
@@ -1,29 +1,43 @@
-require 'themoviedb-api'
-require 'dotenv'
-
 class SearchMovie
-  def initialize(movie_name)
-    @movie_name = movie_name
-    self
+  require 'dotenv/load'
+  def initialize(string)
+    @name = string
+  end
+
+  def auth
+    Tmdb::Api.key(ENV['API_KEY'])
   end
 
   def search
-    Tmdb::Api.key(Rails.application.credentials.dig(:movie_key_v3))
     @search = Tmdb::Search.new
-    # @search.resource('person') # determines type of resource
-    @search.query(@movie_name) # the query to search against
-    # @search.fetch # makes request
-    @results = []
-    @search.fetch.first(20).each do |movie|
-      @results.push(Movie.new(
-                      movie['title'],
-                      movie['release_date'],
-                      Tmdb::Movie.credits(movie['id'])['crew'].select { |x| x['job'] == 'Director' }[0]['name'],
-                      'https://image.tmdb.org/t/p/w220_and_h330_bestv2' + movie['poster_path']
-                    ))
-    rescue StandardError => e
-      puts e.message
-    end
-    @results
+    @search.resource("movie") # determines type of resource
+    @search.query(@name) # the query to search against
+    @hash_result = @search.fetch # makes request
+    @array_cleaned = []
+    @hash_result.each { |movie|
+      hash_movie = {}
+      hash_movie["id"] = movie["id"]
+      hash_movie["title"] = movie["title"]
+      credit = Tmdb::Movie.credits(movie["id"])
+      if !credit.empty?
+        if !credit["crew"].empty?
+          if credit["crew"][0]["name"]
+            director = credit["crew"][0]["name"]
+          end
+        end
+      else director = nil
+      end
+      hash_movie["director"] = director
+      hash_movie["date"] = movie["release_date"]
+      hash_movie["poster"] = movie["poster_path"]
+      @array_cleaned << hash_movie
+    }
+    @array_cleaned
+
+  end
+
+  def perform
+    auth
+    search
   end
 end
